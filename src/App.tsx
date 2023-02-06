618e74b0d5697f01caa00e103d97f409bddbd4d6
shayCamp <76885270+ssutl@users.noreply.github.com>
2023-02-06 12:29:04 +0000

    Initial Layout 🖼️

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./Styling/App.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <h1>Splitify</h1>
+      </div>
+      <div className="description">
+        <h2>Create Vibe Specific Playlists Fast</h2>
+      </div>
+      <div className="albumCovers"></div>
+      <div className="signIn"></div>
     </div>
   );
 }

3c253b9bcde8ffc823240fba3844bb0cae1f773a
shayCamp <76885270+ssutl@users.noreply.github.com>
2023-02-07 22:35:50 +0000

    Seperated by vibes 🎵

--- src/PlaylistsPage.tsx
+++ src/PlaylistsPage.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import "./Styling/PlaylistsPage.scss";
 import SpotifyWebApi from "spotify-web-api-js";
+import vibeSeperator, { resultTemplate } from "./Function/VibeSeperator";
 
 //interface PlaylistsPageProps {}
 
@@ -10,11 +11,15 @@ const PlaylistsPage = () => {
   const [everyTrack, setEveryTrack] = useState<SpotifyApi.TrackObjectFull[]>(
     []
   );
+
+  const [everyTrackLoaded, setEveryTrackLoaded] = useState(false);
   const [audioFeatures, setAudioFeatures] = useState<
     SpotifyApi.AudioFeaturesObject[]
   >([]);
-
-  console.log("audio", audioFeatures);
+  const [audioFeaturesLoaded, setAudioFeaturesLoaded] = useState(false);
+  const [completeArray, setCompleteArray] =
+    useState<SpotifyApi.TrackObjectFull[]>();
+  const [seperatedVibes, setSeperatedVibes] = useState<resultTemplate>();
   const spotify = new SpotifyWebApi();
 
   const getTokenFromUrl = () => {
@@ -35,39 +40,101 @@ const PlaylistsPage = () => {
     if (spotifyToken) {
       spotify.setAccessToken(spotifyToken);
 
-      spotify.getMe().then((user) => {
-        console.log("user.id", user.id);
-      });
-
       spotify.getUserPlaylists().then((playlists) => {
+        const promises: any[] = [];
         playlists.items
           .filter((eachPlaylist) => eachPlaylist.tracks.total !== 0)
           .map((eachPlaylist) => {
             const totalTrackCount = eachPlaylist.tracks.total;
             let currentTrackCount = 0;
-
             while (currentTrackCount < totalTrackCount) {
-              spotify
-                .getPlaylistTracks(eachPlaylist.id, {
-                  limit: 100,
-                  offset: currentTrackCount,
-                })
-                .then((tracks) => {
-                  tracks.items.map((eachTrack) => {
-                    setEveryTrack((everyTrack: any) => [
-                      ...everyTrack,
-                      eachTrack.track,
-                    ]);
-                  });
-                });
+              promises.push(
+                spotify
+                  .getPlaylistTracks(eachPlaylist.id, {
+                    limit: 100,
+                    offset: currentTrackCount,
+                  })
+                  .then((tracks) => {
+                    tracks.items.map((eachTrack) => {
+                      setEveryTrack((everyTrack: any) => [
+                        ...everyTrack,
+                        eachTrack.track,
+                      ]);
+                    });
+                  })
+              );
               currentTrackCount += 100;
             }
           });
+
+        Promise.all(promises).then(() => {
+          // All elements have been added to the everyTrack array
+          setEveryTrackLoaded(true);
+        });
       });
     }
   }, [spotifyToken]);
 
-  return <div className="PlaylistsPage">PlaylistsPage</div>;
+  useEffect(() => {
+    if (everyTrackLoaded) {
+      let bottomPointer = 0;
+      let topPointer = 100;
+      const promises = [];
+
+      const stringArray = everyTrack
+        .filter((eachTrack) => eachTrack !== null)
+        .map((eachTrack) => eachTrack.id);
+
+      while (bottomPointer < everyTrack.length) {
+        const chunk = stringArray.slice(bottomPointer, topPointer);
+        promises.push(
+          spotify.getAudioFeaturesForTracks(chunk).then((results) => {
+            setAudioFeatures((audioFeatures) => [
+              ...audioFeatures,
+              ...results.audio_features,
+            ]);
+          })
+        );
+
+        bottomPointer += 100;
+        topPointer += 100;
+        if (topPointer > everyTrack.length) {
+          topPointer = everyTrack.length;
+        }
+      }
+
+      Promise.all(promises).then(() => {
+        // All elements have been added to the everyTrack array
+        setAudioFeaturesLoaded(true);
+      });
+    }
+  }, [everyTrackLoaded]);
+
+  useEffect(() => {
+    if (audioFeaturesLoaded && everyTrackLoaded) {
+      const mergedArray = everyTrack
+        .filter((eachTrack) => eachTrack !== null)
+        .map((obj1) => {
+          const obj2 = audioFeatures
+            .filter((eachAudioFeature) => eachAudioFeature !== null)
+            .find((o) => o.id === obj1.id);
+          return { ...obj1, audioFeatures: obj2 };
+        });
+      setCompleteArray(mergedArray);
+    }
+  }, [audioFeaturesLoaded, everyTrackLoaded]);
+
+  useEffect(() => {
+    if (completeArray) {
+      setSeperatedVibes(vibeSeperator(completeArray));
+    }
+  }, [completeArray]);
+
+  return (
+    <div className="PlaylistsPage">
+      {seperatedVibes === undefined ? <h1>Loading</h1> : <h1>Loaded</h1>}
+    </div>
+  );
 };
 
 export default PlaylistsPage;
